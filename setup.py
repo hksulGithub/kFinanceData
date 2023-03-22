2411af06e24b89058a586eb9a1a2c496a250514e
hksulGithub <99107270+hksulGithub@users.noreply.github.com>
2023-03-22 18:08:07 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
 
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='kfinancedata',
+    url='https://github.com/hksulGithub/kfinancedata',
+    author='Hong Kee Sul',
+    author_email='hksul@cau.ac.kr',
+    # Needed to actually package something
+    packages=['kfinancedata'],
+    # Needed for dependencies
+    install_requires=['pandas', 'pytz', 'requests'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='Python Package for Korean Financial Market Data',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
