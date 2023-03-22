ce3ae076e2e930f40827a0a7b7d2ff1995878008
hksulGithub <99107270+hksulGithub@users.noreply.github.com>
2023-03-22 18:15:29 +0900

    Update __init__.py

--- kfinancedata/__init__.py
+++ kfinancedata/__init__.py
@@ -1,4 +1,7 @@
 #__init__.py
 
-from kfinancedata import kfinancedataInstance #import the class from file mystuff
+#from kfinancedata import kfinancedataInstance #import the class from file mystuff
 #from kfinancedata import getFutureOptionCodesHeaderData #Import the methods
+
+
+from .kfinancedata import *
